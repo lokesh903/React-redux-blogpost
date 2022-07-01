71a230560a93696eb6a309ee6f0842b7c78e8d61
lokesh903 <lavkeshchhatani@gmail.com>
2022-07-01 19:32:04 +0530

    init commit

---
+++ src/components/EditPost/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div>
+      I am in Edit Post
+    </div>
+  )
+}
+
+export default index
---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function index() {
+  return (
+    <div className='container'>
+        
+      
+    </div>
+  )
+}
+
+export default index
